52fd8fcaf1213969d25c37596e4bbf632bc44f11
Maria Fernanda Rincon <fernandarincon1930@gmail.com>
2025-04-01 21:09:53 -0500

    Actualizacion main

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+#Importar una funcion de tabulate de la libreria de tabulate
+from tabulate import tabulate
+#Funcion en donde se llama menu, y se inicializa la lista_tareas
 def inicio():
     lista_tarea=[]
-    print('--------------Bienvenido-----------------')
+    print('--------------------------Bienvenido--------------------------')
     menu(lista_tarea)
+#funcion en donde termina el programa
 def terminar():
-    print('-----------------End--------------------')
+    print('--------------------------End--------------------------')
+#Funcion en
 def menu(lista_tarea):
-    print('----------------Menu---------------------')
+    print('--------------------------Menu--------------------------')
     print('\n1. Agregar tarea nueva\n2. Eliminar tarea\n3. Ver tarea\n4. Marcar como completada una tarea\n0. Salir')
     opcion=int(input('Seleccione la opcion que desea: '))
     if opcion==1:
@@ -26,13 +31,14 @@ def menu(lista_tarea):
         print('Opcion incorrecta. Eliga nuevamente una opcion valida')
         menu(lista_tarea)
     
-def agregar_tarea(lista_tarea): # type: ignore
-    print('--------------Agregar tarea---------------------')
+def agregar_tarea(lista_tarea):
+    tamano=len(lista_tarea)
+    print('--------------------------Agregar tarea--------------------------')
     titulo=input('Ingrese el titulo de la tareas: ')
     descripcion=input('Ingrese la descripcion de la tarea: ')
-    estado=0
-    lista_tarea.append([titulo,descripcion,estado])
-    print('------------------------------------------------')
+    estado='Pendiente'
+    lista_tarea.append([tamano+1,titulo,descripcion,estado])
+    print('-----------------------------------------------------------------')
     print('1. Agregar otra tarea\n2. Ir a menu')
     opcion=int(input('Ingrese la opcion que desea: '))
     if opcion==1:
@@ -41,26 +47,34 @@ def agregar_tarea(lista_tarea): # type: ignore
 def ver_tareas(lista_tareas):
     tamano=len(lista_tareas)
     print('-------------------Lista de tareas---------------------------')
-    print(f"id      Titulo                      Descripcion                     Estado")
+    print(f"id      Titulo                        Descripcion                           Estado")
     for i in range(tamano):
-        titulo=lista_tareas[i][0]
-        descripcion=lista_tareas[i][1]
-        estado="Completado" if lista_tareas[i][2]==1 else "Pendiente" 
+        id=lista_tareas[i][0]
+        titulo=lista_tareas[i][1]
+        descripcion=lista_tareas[i][2]
+        estado="Completado" if lista_tareas[i][3]==1 else "Pendiente" 
         print(f"{i+1}     {titulo}        {descripcion}      {estado}")
-    
+    print(tabulate(lista_tareas,headers=['Id','Titulo','Descripcion','Estado'],tablefmt="grid"))
 def eliminar_tarea(lista_tareas):
     ver_tareas(lista_tareas)
     print('---------------------Eliminar tarea--------------------------')
     lista_borrar=int(input('Ingresa el id de la tarea que deseas borrar '))-1
     #Agregar estar seguro de borrar esta lista
-    lista_tareas.pop(lista_borrar)
-    print(lista_tareas)
+    if lista_borrar<len(lista_tareas) and lista_borrar>-1:
+        lista_tareas.pop(lista_borrar)
+    else:
+        print('Ingrese un valor valido')
+        eliminar_tarea(lista_tareas)
     return lista_tareas
 def marcar_completada(lista_tareas):
     ver_tareas(lista_tareas)
-    print('-----------------Marcar compleatda tarea---------------------')
+    print('--------------------------Marcar completada tarea--------------------------')
     tarea_cambio_estado=int(input('Ingresa el id de la tarea que deseas cambiar el estado a Completado '))-1
-    lista_tareas[tarea_cambio_estado][2]=1
+    if tarea_cambio_estado < len(lista_tareas) and tarea_cambio_estado>-1:
+        lista_tareas[tarea_cambio_estado][3]='Completado'
+    else:
+        print('Ingrese un valor valido')
+        marcar_completada(lista_tareas)
     ver_tareas(lista_tareas)
     return lista_tareas
 inicio()
---
+++ readme.md
@@ -0,0 +1,11 @@
+#Sistema de gestion de tareas
+
+Descripción: El objetivo de este proyecto es crear un sistema simple de gestión
+de tareas donde los usuarios puedan agregar, eliminar, y ver tareas. Cada tarea tendrá
+un título, una descripción y un estado (pendiente o completada).
+
+Requisitos:
+Variables: Para almacenar información de los gastos.
+Listas: Para mantener un registro de todos los gastos.
+Condicionales: Para manejar las opciones del menú y las categorías de gastos.
+Ciclos: Para permitir múltiples entradas de gastos y mostrar el menú hasta que el usuario decida salir.
